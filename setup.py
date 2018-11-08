fc71df7d3f4591194dd483e253dd7988746cc2ae
DyLanCao <caoyin2011@163.com>
2018-11-08 20:20:32 +0800

    add audio base on

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CoolAudio',
+      version='0.0.1',
+      description='Python Audio Analysis Library: Feature Extraction, Classification, Segmentation and Applications',
+      url='https://github.com/DyLanCao/CoolAdudio',
+      author='DyLan cao',
+      author_email='caoyin2011@163.com',
+      license='Apache License, Version 2.0',
+      packages=['CoolAudio'],
+      zip_safe=False)
